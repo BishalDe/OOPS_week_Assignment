5f3e92a1c472079d5cab821d8b223c77ff83f68a
BishalDe <itsbishalde@gmail.com>
2022-08-17 00:24:35 +0530

    done 9

---
+++ 9.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i, j, row, column;
+    int **arr;
+    cout << "Enter The Row : ";
+    cin >> row;
+    cout << "Enter The Columns : ";
+    cin >> column;
+
+    arr = new int *[row];
+    for (i = 0; i < row; i++)
+    {
+        arr[i] = new int[column];
+    }
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            arr[i][j] = i * j;
+        }
+    }
+    cout << "[";
+    for (i = 0; i < row; i++)
+    {
+        cout << "[";
+        for (j = 0; j < column; j++)
+        {
+            cout << arr[i][j];
+            if (j != column - 1)
+            {
+                cout << ",";
+            }
+        }
+        cout << "]";
+        if (i != row - 1)
+        {
+            cout << ",";
+        }
+    }
+    cout << "]";
+
+    return 0;
+}
