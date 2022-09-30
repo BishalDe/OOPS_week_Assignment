46fe8b204ee7535a62006fe09ed07f19f98b8ab2
BishalDe <itsbishalde@gmail.com>
2022-09-30 23:58:38 +0530

    final added

--- OOPS_6&7/7.cpp
+++ OOPS_6&7/7.cpp
@@ -28,7 +28,13 @@ class OpComplex{
             A.real = real * obj.real;  
             A.imaginary = imaginary * obj.imaginary;  
             return A;  
-        }          
+        }       
+        OpComplex operator / (OpComplex obj){   
+            OpComplex A;  
+            A.real = real / obj.real;  
+            A.imaginary = imaginary / obj.imaginary;  
+            return A;  
+        }    
         void add()  
         {  
             cout << real << "+" << imaginary << "j" << "\n";  
@@ -44,11 +50,15 @@ class OpComplex{
         {  
             cout << real << "+" << imaginary << "j" << "\n";  
         } 
+        void div()  
+        {  
+            cout << real << "+" << imaginary << "j" << "\n";  
+        } 
 };
 
 int main ()  
 {  
-    OpComplex n1, n2, sum, sub, mul;
+    OpComplex n1, n2, sum, sub, mul,div;
     cout<<"Enter The First Complex Number {Real <space> Imaginary part} : ";
     n1.getval();
     cout<<"Enter The Second Complex Number {Real <space> Imaginary part} : "; 
@@ -57,6 +67,7 @@ int main ()
     sum = n1 + n2;
     sub = n1 - n2; 
     mul = n1 * n2;
+    div = n1 / n2;
 
     cout << "\nThe addition of two complex (real and imaginary) numbers : ";  
     sum.add();    
@@ -64,6 +75,8 @@ int main ()
     sub.sub(); 
     cout << "\nThe multiplication of two complex (real and imaginary) numbers : ";  
     mul.mul(); 
+    cout << "\nThe Division of two complex (real and imaginary) numbers : ";
+    div.div();
     cout<<endl;
     return 0;  
 }  
