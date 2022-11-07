744eca3d6c237cd6f0531800b3215385a22719c6
BishalDe <itsbishalde@gmail.com>
2022-11-07 15:13:23 +0530

    Week 9 added

---
+++ OOPS_8/2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+class base {
+public:
+virtual void show(){
+cout << "show base class" << endl;
+}
+void print(){
+cout << "print base class" << endl;
+}
+};
+class derived : public base {
+public:
+void show(){
+cout << "show derived class" << endl;
+}
+void print(){
+cout << "print derived class" << endl;
+}
+
+};
+int main(){
+base* bpointr;
+derived dev;
+bpointr = &dev;
+bpointr->show();
+bpointr->print();
+return 0;
+}
